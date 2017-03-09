e1dcfa1f2fa7dc020801dc23e2fa29f843e531bf
Jean Robert <jean.alves@ntconsult.com.br>
2017-03-09 13:34:12 -0300

    teste de animacao de morte

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour {
     public bool isMoving;
     public float moveSpeed;
     public bool isLookingUp;
+    public float timeToDeath;
+    public float timeDeathAnimation;
+    public bool isDead;
 
     protected Vector3 targetPosition;
     protected float horizontalAxis;
@@ -15,17 +18,40 @@ public class PlayerController : MonoBehaviour {
     protected Vector2 currentVelocity;
     protected AimHelper.AimAngle currentAimAngle;
     protected AimHelper.AimAngle lastAimAngle;
-    protected Animator myAnimator;
+    protected Animator myAnimator;    
+    protected float timeToDeathCounter;
+    protected float timeDeathAnimationCounter;
+
+    
+    protected Vector2 lastMoveInput;
 
 
     // Use this for initialization
     void Start () {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        timeToDeathCounter = timeToDeath;
+        isDead = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if (isDead) {
+            timeDeathAnimationCounter -= Time.deltaTime;
+            if(timeDeathAnimationCounter > 0) {
+                currentVelocity = new Vector2(lastMoveInput.x * -1, lastMoveInput.y * -1) * moveSpeed;
+            } else {
+                currentVelocity = Vector2.zero;
+            }
+        } else {
+            timeToDeathCounter -= Time.deltaTime;
+            if (timeToDeathCounter <= 0) {
+                isDead = !isDead;
+                timeToDeathCounter = timeToDeath;
+                timeDeathAnimationCounter = timeDeathAnimation;
+            }
+        }
+
         UpdateMouseKeyboardInputs();
         UpdateMovement();
         UpdateSprites();
@@ -35,38 +61,46 @@ public class PlayerController : MonoBehaviour {
 
     public virtual void UpdateMovement()
     {
-        //move
-        if (horizontalAxis != 0 || verticalAxis != 0)
-        {
-            isMoving = true;
-        }
-        else
+        if(isDead)                          
         {
             isMoving = false;
-        }
+            
+        } else {
+            //move
+            if (horizontalAxis != 0 || verticalAxis != 0)
+            {
+                isMoving = true;
+            }
+            else
+            {
+                isMoving = false;
+            }
 
-        if (isMoving)
-        {
-            var moveInput = new Vector2(horizontalAxis, verticalAxis);
-            currentVelocity = moveInput * moveSpeed;
-        }
-        else
-        {
-            currentVelocity = Vector2.zero;
+            if (isMoving)
+            {
+                lastMoveInput = new Vector2(horizontalAxis, verticalAxis);
+                currentVelocity = lastMoveInput * moveSpeed;
+            }
+            else
+            {
+                currentVelocity = Vector2.zero;
+            }
         }
     }
 
     private void UpdateSprites()
     {
-        currentAimAngle = AimHelper.GetCurrentAimAngle(targetPosition, transform.position);
+        if (!isDead) {
+            currentAimAngle = AimHelper.GetCurrentAimAngle(targetPosition, transform.position);
 
-        if (currentAimAngle != lastAimAngle)
-        {
-            isLookingUp = currentAimAngle.y == Enums.Angles.Y.Top;
-
-            if ((lastAimAngle == null && currentAimAngle.x == Enums.Angles.X.Left) || (lastAimAngle != null && currentAimAngle.x != lastAimAngle.x))
+            if (currentAimAngle != lastAimAngle)
             {
-                Flip();
+                isLookingUp = currentAimAngle.y == Enums.Angles.Y.Top;
+
+                if ((lastAimAngle == null && currentAimAngle.x == Enums.Angles.X.Left) || (lastAimAngle != null && currentAimAngle.x != lastAimAngle.x))
+                {
+                    Flip();
+                }
             }
         }
     }
@@ -76,6 +110,7 @@ public class PlayerController : MonoBehaviour {
         lastAimAngle = currentAimAngle;
         myAnimator.SetBool("isMoving", isMoving);
         myAnimator.SetBool("isLookingUp", isLookingUp);
+        myAnimator.SetBool("isDead", isDead);
     }
 
     protected virtual void Flip()
